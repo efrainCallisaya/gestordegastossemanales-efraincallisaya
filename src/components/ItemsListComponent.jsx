583bb4ec27e0a14b7342ce7b27d70da07c1470e0
efrainCallisaya <efra.man.calli.hino@gmail.com>
2022-06-28 12:03:32 -0300

    mejoras a nivel de estructura + entrega del desafio catalogo con maps y promises

--- src/components/ItemsListComponent.jsx
+++ src/components/ItemsListComponent.jsx
@@ -1,11 +1,15 @@
-import ItemCount from './ItemListContainer/ItemCount.js'
+import ItemCount from './ItemCount'
+import { ItemList } from './ItemList';
+
 const ejericio = ({componentesI})=>{
     return (
         <>
         <ItemCount stock ="5" inicial="1"/>
         <div >
             {componentesI}
+
         </div>
+        <ItemList />
         </>
     )
 }
