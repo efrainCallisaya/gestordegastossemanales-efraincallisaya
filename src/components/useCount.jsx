fdbb614b5000625040a69294d14bc7133e47e233
efrainCallisaya <efra.man.calli.hino@gmail.com>
2022-07-21 20:49:18 -0300

    se elimina la carpeta Mocks

--- src/components/useCount.jsx
+++ src/components/useCount.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-const useCount = (stock, inicial) => {
-  const [contador, setContador] = useState(parseInt(inicial));
+const useCount = (stock) => {
+  const [contador, setContador] = useState(parseInt(1));
 
   const funcionlogica = (valor) => {
     const result = contador + valor;
 
-    if (valor === 0) {
-      setContador(0);
-    } else if (result <= stock && result >= 0) {
+ if (result <= stock && result >= 0) {
       setContador(contador + valor);
     }
   };
